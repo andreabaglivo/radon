b14635f9854fd09bce2d5960170d3a5bfca13ebb
Andrea <andreabaglivo@gmail.com>
2023-12-26 17:00:30 +0100

    mappa fixata

--- src/app/shared/map/map.component.ts
+++ src/app/shared/map/map.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'ae-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
 
+  ngOnInit(): void {
+    const map = L.map('map').setView([45.65, 13.75], 12);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+  }
 }
