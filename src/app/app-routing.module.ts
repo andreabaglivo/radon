914b30969df1ad3242915ac0ae372d4e90599c82
Andrea <andreabaglivo@gmail.com>
2024-05-26 17:27:47 +0200

    base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,61 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { BuildingComponent } from './pages/building/building.component';
-import { RoomComponent } from './pages/room/room.component';
-import { ElectricComponent } from './pages/electric/electric.component';
-import { PhotovoltaicComponent } from './pages/photovoltaic/photovoltaic.component';
-import { StructureComponent } from './pages/structure/structure.component';
-import { HeatingComponent } from './pages/heating/heating.component';
-import { GardenComponent } from './pages/garden/garden.component';
-import { HydraulicComponent } from './pages/hydraulic/hydraulic.component';
-import { InsuranceComponent } from './pages/insurance/insurance.component';
-import { ExpenseComponent } from './pages/expense/expense.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-  },
-  {
-    path: 'building',
-    component: BuildingComponent,
-  },
-  {
-    path: 'building/room',
-    component: RoomComponent,
-  },
-  {
-    path: 'building/electric',
-    component: ElectricComponent,
-  },
-  {
-    path: 'building/photovoltaic',
-    component: PhotovoltaicComponent,
-  },
-  {
-    path: 'building/structure',
-    component: StructureComponent,
-  },
-  {
-    path: 'building/heating',
-    component: HeatingComponent,
-  },
-  {
-    path: 'building/garden',
-    component: GardenComponent,
-  },
-  {
-    path: 'building/hydraulic',
-    component: HydraulicComponent,
-  },
-  {
-    path: 'building/expense',
-    component: ExpenseComponent,
-  },
-  {
-    path: 'building/insurance',
-    component: InsuranceComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,17 +11,7 @@ import {MatButtonModule} from '@angular/material/button';
 import { MapComponent } from './shared/map/map.component';
 import {MatInputModule} from '@angular/material/input';
 import { SearchMenuComponent } from './shared/search-menu/search-menu.component';
-import { BuildingComponent } from './pages/building/building.component';
-import { RoomComponent } from './pages/room/room.component';
-import { ElectricComponent } from './pages/electric/electric.component';
 import { SheetComponent } from './shared/sheet/sheet.component';
-import { HydraulicComponent } from './pages/hydraulic/hydraulic.component';
-import { StructureComponent } from './pages/structure/structure.component';
-import { PhotovoltaicComponent } from './pages/photovoltaic/photovoltaic.component';
-import { HeatingComponent } from './pages/heating/heating.component';
-import { GardenComponent } from './pages/garden/garden.component';
-import { InsuranceComponent } from './pages/insurance/insurance.component';
-import { ExpenseComponent } from './pages/expense/expense.component';
 
 @NgModule({
   declarations: [
@@ -31,17 +21,7 @@ import { ExpenseComponent } from './pages/expense/expense.component';
     CardComponent,
     MapComponent,
     SearchMenuComponent,
-    BuildingComponent,
-    RoomComponent,
-    ElectricComponent,
     SheetComponent,
-    HydraulicComponent,
-    StructureComponent,
-    PhotovoltaicComponent,
-    HeatingComponent,
-    GardenComponent,
-    ExpenseComponent,
-    InsuranceComponent,
   ],
   imports: [
     BrowserModule,
